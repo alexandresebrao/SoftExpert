356a8803c2114079445a8d59d7920a23ca736e86
Alexandre Sebrão <alexandre.sebrao@gmail.com>
2017-05-17 10:27:01 -0300

    WEB-303 cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import '../css/bootstrap.css';
 import '../css/main.css';
-import 'react-hot-loader/patch';
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -8,12 +8,7 @@ import { createStore } from 'redux'
 import todos from './reducers/todos'
 import App from './view.js'
 
-if (module.hot) {
-  module.hot.accept('./view.js', () => {
-    const NextRootContainer = require('./view.js').default;
-    ReactDOM.render(<App />,document.getElementById('root'));
-  });
-}
+
 
 ReactDOM.render(
     <App />,
